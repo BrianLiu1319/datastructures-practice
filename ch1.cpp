cb8423a507dd8f44877b8add4c8d31b137bb2eaa
BrianLiu1319 <brianliu1337@yahoo.com>
2022-12-19 13:12:24 -0800

    com

---
+++ ch1.cpp
@@ -0,0 +1,16 @@
+/*
+
+
+*/
+
+#include <iostream>
+
+int main() {
+
+  std::cout << "hello worlddddd, what the brown" << std::endl;
+
+  std::cout << "why wont this output??" << std::endl;
+  std::cout << "hello";
+  std::cout << "test" << std::endl;
+
+}
